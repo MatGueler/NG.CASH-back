5a1ba1d9ea3d0218799962ed33fb3be1d46e585b
MattGueler <mateus.gueler@gmail.com>
2022-11-18 15:49:55 -0300

    :pencil: feat:
    Create login schema to validate body informations

--- src/Routers/UserRouter.ts
+++ src/Routers/UserRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { RegisterUser } from "../Controller/UserController";
+import { validateSchema } from "../Middlewares/ValidateSchemaMiddleware";
+import registerSchema from "../Schemas/LoginSchema";
 
 const userRouter = Router();
 
-userRouter.post("/sign-up", RegisterUser);
+userRouter.post("/sign-up", validateSchema(registerSchema), RegisterUser);
 
 export default userRouter;
